2ef0a6f37a99853734c27d2dd999842baf2c4c51
GARAMAS <ramsai.gadagottu@gmail.com>
2022-05-26 12:39:09 +0530

    Initial setup

---
+++ src/components/home/landingPage.jsx
@@ -0,0 +1,15 @@
+import { AppBar, Toolbar } from "@material-ui/core";
+import React from "react";
+
+const LandingPage = () =>{
+
+    return (
+        <header>
+            <AppBar>
+                <Toolbar>eProfile Experts</Toolbar>
+            </AppBar>
+        </header>
+    )
+}
+
+export default LandingPage;
